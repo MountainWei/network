bc3187a2f1750924ba8bf7efdca5c8a5dae2578f
liu wei <mountainwei@outlook.com>
2016-01-13 16:11:26 +0800

    modify sockerClient

--- simpleSocket/socket-client.py
+++ simpleSocket/socket-client.py
@@ -6,7 +6,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 while 1:
     data = raw_input('Please input data:')
-    s.sendall(data)
-    data = s.recv(1024)
-    print data
+    if data != 'q':
+        s.sendall(data)
+        data = s.recv(1024)
+        print data
+    else:
+        break
 s.close()
